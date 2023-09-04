be22e6e9df6ac351ff1c9aa0c60bd5226bea06c0
qqqwwwcg <qw_191265402@163.com>
2023-09-04 13:12:41 +0800

    add window

--- raster/src/main.rs
+++ raster/src/main.rs
@@ -1,3 +1,11 @@
+use fltk::{prelude::*, window::Window};
+
+const WIDTH:i32 = 1200;
+const HEIGHT:i32 = 800;
+
 fn main() {
-    println!("Hello, world!");
+let app = fltk::app::App::default();
+let window = Window::new(100,100,WIDTH,HEIGHT,"raster");
+
+app.run().unwrap();
 }
