24810e5f521eac6b05d22557a6df9863928d500c
qqqwwwcg <qw_191265402@163.com>
2023-09-10 20:40:07 +0800

    note

--- raster/src/main.rs
+++ raster/src/main.rs
@@ -3,9 +3,16 @@ use fltk::{prelude::*, window::Window};
 const WIDTH:i32 = 1200;
 const HEIGHT:i32 = 800;
 
+//app: application for run event loop
+//window: surface and persation image
+
 fn main() {
 let app = fltk::app::App::default();
-let window = Window::new(100,100,WIDTH,HEIGHT,"raster");
+let mut window = Window::new(100,100,WIDTH,HEIGHT,"raster");
 
+window.end();
+window.show();
+
+//run event loop
 app.run().unwrap();
 }
