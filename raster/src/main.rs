3ca06bb459f58e11280e81997de2e2b3d193dcba
qqqwwwcg <qw_191265402@163.com>
2023-09-23 11:51:06 +0800

    draw image on window

--- raster/src/main.rs
+++ raster/src/main.rs
@@ -10,6 +10,19 @@ fn main() {
 let app = fltk::app::App::default();
 let mut window = Window::new(100,100,WIDTH,HEIGHT,"raster");
 
+let data:Vec<u8> = (0..WIDTH*HEIGHT*3).into_iter().map(|channle|channle as u8).collect();
+window.draw(move |_| {
+    fltk::draw::draw_image(
+        &data,
+        0,
+        0,
+        WIDTH ,
+        HEIGHT ,
+        fltk::enums::ColorDepth::Rgb8,
+    )
+    .unwrap();
+});
+
 window.end();
 window.show();
 
